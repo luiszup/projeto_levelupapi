eff55ac7911645d6e9226a8388b2b96c8cb5f700
igor alves <igoralvespalmeirag4@gmail.com>
2025-05-15 11:55:41 -0300

    inicial

---
+++ src/main/java/com/projeto/levelupapi/projeto_levelupapi/ProjetoLevelupapiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.levelupapi.projeto_levelupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLevelupapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLevelupapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/levelupapi/projeto_levelupapi/ProjetoLevelupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.levelupapi.projeto_levelupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLevelupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
