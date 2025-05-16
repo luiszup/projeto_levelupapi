d1fabf40708579f117908fae8a6e32987f7a69f5
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-05-16 14:28:01 -0300

    feat: cria classe de serviço para item com método criaritem

---
+++ src/main/java/com/projeto/levelupapi/projeto_levelupapi/service/ItemService.java
@@ -0,0 +1,23 @@
+package com.projeto.levelupapi.projeto_levelupapi.service;
+
+import com.projeto.levelupapi.projeto_levelupapi.model.Item;
+import com.projeto.levelupapi.projeto_levelupapi.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public Item criarItem(String name, String description) {
+        if (itemRepository.procurarPorNome(name).isPresent()) {
+            throw new RuntimeException("O item já existe");
+        }
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        return itemRepository.save(item);
+    }
+}
