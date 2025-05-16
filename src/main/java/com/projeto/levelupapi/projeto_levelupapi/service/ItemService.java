15ef56b1e3b9b317b736ea917ef9413809b579c8
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-05-16 14:34:08 -0300

    feat: cria método procurarpornome em itemservice

--- src/main/java/com/projeto/levelupapi/projeto_levelupapi/service/ItemService.java
+++ src/main/java/com/projeto/levelupapi/projeto_levelupapi/service/ItemService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
@@ -25,5 +26,9 @@ public class ItemService {
 
     public List<Item> listarTodos() {
         return itemRepository.findAll();
+    }
+
+    public Optional<Item> procurarPorNome(String name) {
+        return itemRepository.procurarPorNome(name);
     }
 }
