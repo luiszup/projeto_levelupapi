575057b237ab8779f03167221685d25d045266d0
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-05-16 14:58:41 -0300

    feat: cria classe controller de item com método criaritem

---
+++ src/main/java/com/projeto/levelupapi/projeto_levelupapi/controller/ItemController.java
@@ -0,0 +1,29 @@
+package com.projeto.levelupapi.projeto_levelupapi.controller;
+
+import com.projeto.levelupapi.projeto_levelupapi.model.Item;
+import com.projeto.levelupapi.projeto_levelupapi.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping
+    public ResponseEntity<Item> criarItem (@RequestBody Map<String, String> body) {
+        Item item = itemService.criarItem(
+                body.get("name"),
+                body.get("description")
+        );
+        return ResponseEntity.ok(item);
+    }
+}
