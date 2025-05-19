5cb29568f5be2b4c468a6346a266ca2926e8450a
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-05-19 11:45:37 -0300

    feat: cria classe jwttokenprovider com método generatetoken

---
+++ src/main/java/com/projeto/levelupapi/projeto_levelupapi/infra/jwt/JwtTokenProvider.java
@@ -0,0 +1,28 @@
+package com.projeto.levelupapi.projeto_levelupapi.infra.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final String jwtSecret = "chave_secreta";
+    private final int jwtExpirationMs = 86400000;
+
+    public String generateToken(Authentication authentication) {
+        User userPrincipal = (User) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .claim("role", userPrincipal.getAuthorities().iterator().next().getAuthority())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+}
