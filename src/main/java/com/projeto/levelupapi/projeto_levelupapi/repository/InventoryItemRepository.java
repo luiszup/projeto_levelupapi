43071881fc743d7ef7279fe443f58d45c9920549
igor alves <igoralvespalmeirag4@gmail.com>
2025-05-21 05:53:44 -0300

    fix(inventário): Corrigindo bug no inventário

--- src/main/java/com/projeto/levelupapi/projeto_levelupapi/repository/InventoryItemRepository.java
+++ src/main/java/com/projeto/levelupapi/projeto_levelupapi/repository/InventoryItemRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
-    List<InventoryItem> procurarPorJogador(User user);
-    Optional<InventoryItem> procurarPorJogadorEItem(User user, Item item);
+    List<InventoryItem> findByUser(User user);
+    Optional<InventoryItem> findByUserAndItem(User user, Item item);
 }
